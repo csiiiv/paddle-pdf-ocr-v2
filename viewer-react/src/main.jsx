25b0971ab1bf73ed4e66e43f397f4574caea46b4
csiiiv <cjs3villa@gmail.com>
2026-08-25 05:11:06 +0800

    Compact header controls on mobile landscape (#1)
    * Add compact mobile landscape header styles
    
    * Load mobile landscape header overrides

--- viewer-react/src/main.jsx
+++ viewer-react/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 import App from "./App.jsx";
 import "./styles.css";
+import "./mobile-landscape.css";
 import "./pdf-status.css";
 import "./token-geometry.css";
 
